7c043bf16fd074221cb548ab2bd74f461d7110c0
Humberto <matheushumberto520@gmail.com>
2023-06-21 19:48:17 -0400

    Feat: create Transactions page and Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
+import Logo from "../../assets/logo.svg"
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={Logo} alt="" />
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
