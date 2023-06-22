8cc4eff2a003729e5b9ca8249508422f4730556a
Humberto <matheushumberto520@gmail.com>
2023-06-21 20:52:23 -0400

    Feat: create Summary and TransactionsTable  component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
 import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles"
 
 export const Transactions = () => {
   return (
     <div>
       <Header/>
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+                </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Galpões</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  R$ -59,00
+                </PriceHighLight>
+              </td>  
+              <td>Fundos</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
